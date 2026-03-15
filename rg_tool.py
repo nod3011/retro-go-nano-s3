6630a750857f2ab0156c00f3e2a748d20a40b6b8
Nochii <nod3011@hotmail.com>
2026-03-15 15:24:20 +0700

    feat: resize partitions to optimize flash footprint

--- rg_tool.py
+++ rg_tool.py
@@ -19,16 +19,16 @@ PROJECT_NAME = os.getenv("PROJECT_NAME", "Retro-Go")
 PROJECT_ICON = os.getenv("PROJECT_ICON", "assets/icon.raw")
 PROJECT_APPS = {
   # Project name  Type, SubType, Size (bytes)
-  'launcher':     [0, 16, 2097152],  # 2.0 MB (Was 1.5)
-  'retro-core':   [0, 16, 2097152],  # 2.0 MB
-  'gnuboy':       [0, 16, 1572864],  # 1.5 MB
-  'snes9x':       [0, 16, 1572864],  # 1.5 MB
-  'prboom-go':    [0, 16, 1572864],  # 1.5 MB (Was 1.0)
-  'gwenesis':     [0, 16, 1572864],  # 1.5 MB
+  'launcher':     [0, 16, 1441792],  # 1.375 MB
+  'retro-core':   [0, 16, 1376256],  # 1.31 MB
+  'gnuboy':       [0, 16, 1179648],  # 1.125 MB
+  'snes9x':       [0, 16, 851968],   # 832 KB
+  'prboom-go':    [0, 16, 1048576],  # 1.0 MB
+  'gwenesis':     [0, 16, 1179648],  # 1.125 MB
   'fmsx':         [0, 16, 1048576],  # 1 MB
-  'ws':           [0, 16, 1048576],  # 1 MB (Was 0.75)
-  'oswan':        [0, 16, 1048576],  # 1 MB
-  'ngp':          [0, 16, 1048576],  # 1 MB (Was 0.75)
+  'ws':           [0, 16, 1048576],  # 1 MB
+  'oswan':        [0, 16, 655360],   # 640 KB
+  'ngp':          [0, 16, 720896],   # 704 KB
   'quake-go':     [0, 16, 2097152],  # 2 MB (Quake needs more space)
 }
 
